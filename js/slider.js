efbc8d3b0a1072dc8f41b597e1b347e44da629de
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-08-29 02:43:34 +0300

    add-slider

---
+++ js/slider.js
@@ -0,0 +1,38 @@
+//! slider start
+let slideIndex = 1;
+showSlides(slideIndex);
+
+setInterval(() => {
+    showSlides((slideIndex += 1));
+}, 1000);
+
+function plusSlide(n) {
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slider-item");
+    const dots = document.getElementsByClassName("slider-dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+
+
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+//! slider end
